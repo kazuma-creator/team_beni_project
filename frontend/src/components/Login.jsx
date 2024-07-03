77587f929334e5a57f1167e26c7f6d3447ea7706
kazuma <kazumamiyoshi0311@gmail.com>
2024-07-03 14:01:15 +0900

    commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,13 @@
 import React,{useState} from 'react';
 
 const Login = () =>{
-  
+  const [userId,serUserId] = userState('');
+  const [password,setPassword] = userState('');
+
+  const handleLogin = () =>{
+    //ログイン処理
+  };
+  const handleRegister = () =>{
+    //新規登録処理
+  };
 }
