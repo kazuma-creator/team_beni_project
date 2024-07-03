dd30799d94d8351609c04e6756290eb357897881
kazuma <kazumamiyoshi0311@gmail.com>
2024-07-03 09:59:09 +0900

    front back 統合デプロイ

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,12 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='../frontend/build',static_url_path='/')
 
 @app.route('/')
+def serve():
+  return send_from_directory(app.static_folder,'index.html')
+
+
 def home():
     return jsonify({"message": "Welcome to the Flask backend!"})
 
