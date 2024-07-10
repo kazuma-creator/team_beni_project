9012397b206cdab480767768ce79da247e25e26c
mzmtharu <kd1335792@st.kobedenshi.ac.jp>
2024-07-10 15:00:16 +0900

    提出

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+const Header= () => {
+	
+	//必要な機能はここに追加してー
+
+
+  return (
+	//ここにHTML的な感じで書く！
+    <div>
+        <img src=""></img>
+        <div>
+
+            <div>
+                
+            </div>
+
+        </div>
+
+        <bottun>コミュニティを追加</bottun>
+    </div>
+
+     );
+};
+
+
+const styles = {
+	//ここにCSSを書く！
+};
+
+
+export default Header;
